a5d571d986140530f7c020ba469392b2bed8a561
Alfred <akojo2273@gmail.com>
2024-09-10 11:57:37 +0000

    "Added basic_operations.py files"

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+
+sum_result = number1 + number2
+print("Addition of", number1, "and", number2 ,"is", sum_result)
+
+difference_result = number1 - number2
+print("Subtraction of", number1, "and", number2, "is", difference_result)
+
+product_result = number1 * number2
+print("Product of", number1, "and", number2, "is", product_result)
