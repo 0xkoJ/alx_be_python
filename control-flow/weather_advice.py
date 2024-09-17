0f73d412d0013ca875f60a2fa9de7a9c94f55cbc
Alfred <akojo2273@gmail.com>
2024-09-17 11:40:47 +0000

    "Added weather_advice.py"

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,9 @@
+weather_advice = { "sunny" : "Wear a t-shirt and sunglasses." ,
+"rainy" : "Don't forget your umbrella and a raincoat." , "cold" :
+"Make sure to wear a warm coat and a scarf." }
+
+current_weather = input(" What's the weather like today? :")
+
+print("Wear a t-shirt and sunglasses.")
+
+ 
