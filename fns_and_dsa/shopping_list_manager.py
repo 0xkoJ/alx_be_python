68708016791e98898ccb2bb0604841d7a85d6659
Alfred <akojo2273@gmail.com>
2024-09-25 18:50:37 +0000

    "Added shopping_list_manager.py file"

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,24 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View Item")
+    print("4. Exit")
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+        if choice == '1':
+            print(f"Item has been added")
+        elif choice == '2':
+            print(f"Item has been removed from list")
+        elif choice == '3':
+            print(f"Shopping list")
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again")
+if __name__ == "__main__":
+    main()
