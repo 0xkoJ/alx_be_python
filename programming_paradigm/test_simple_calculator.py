1db030ce86f68a94e37e6e464598dc3b5eafc86b
Alfred <akojo2273@gmail.com>
2024-10-04 06:52:33 +0000

    "Update test_simple_calculator.py file"

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -30,7 +30,7 @@ class TestSimpleCalculator(unittest.TestCase):
        self.assertEqual(self.calc.multiply(-3, -2), 6)
        self.assertEqual(self.calc.multiply(1.5, 2), 3.0)
 
-    def test_divide(self):
+    def test_division(self):
        """Test the division method"""
        self.assertEqual(self.calc.divide(10, 2), 5)
        self.assertEqual(self.calc.divide(9, 3), 3)
